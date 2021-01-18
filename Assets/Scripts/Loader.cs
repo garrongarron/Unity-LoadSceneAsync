01ba4d39f0de8210d191f754e61518813cf4b32c
Federico Zacayan <federico.zacayan@gmail.com>
2021-01-18 11:41:13 -0300

    First Commit

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+    void Start()
+    {
+          
+    }
+
+    // Update is called once per frame
+    public void Load2()
+    {
+        scenesToLoad.Add(SceneManager.LoadSceneAsync("Scene2", LoadSceneMode.Additive));
+        if(SceneManager.GetSceneByName("Scene3").isLoaded){
+            SceneManager.UnloadSceneAsync("Scene3");
+        }
+    }
+    public void Load3()
+    {
+        scenesToLoad.Add(SceneManager.LoadSceneAsync("Scene3", LoadSceneMode.Additive));
+        if(SceneManager.GetSceneByName("Scene2").isLoaded){
+            SceneManager.UnloadSceneAsync("Scene2");
+        }
+        
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        float totalProgress = 0;
+        for (int i = 0; i < scenesToLoad.Count; i++)
+        {
+            while (!scenesToLoad[i].isDone)
+            {
+                totalProgress += scenesToLoad[i].progress;
+                Debug.Log(totalProgress/scenesToLoad.Count);
+                yield return null;
+            }
+        }
+    }
+}
